a7bed5084428fb7d0209813eb43519534e216d5e
Gresa Smolica <gresasmolica@gmail.com>
2023-03-20 01:56:05 +0100

    final edits

--- ArrayDeque.py
+++ ArrayDeque.py
@@ -4,13 +4,96 @@ Created on Sun Mar  5 18:34:01 2023
 
 @author: Hannah
 """
+class Empty(Exception):
+    pass
 
-class Empty(Exception): pass
+class ArrayDequeMaxlen:
+    def __init__(self, maxlen):
+        self._data = [None] * maxlen
+        self._size = 0
+        self._front = 0
+        self._maxlen = maxlen
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._data[self._front]
+
+    def last(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        back = (self._front + self._size - 1) % self._maxlen
+        return self._data[back]
+
+    def append(self, e):
+        if self._size == self._maxlen:
+            raise Exception('Deque is full')
+        back = (self._front + self._size) % self._maxlen
+        self._data[back] = e
+        self._size += 1
+
+    def appendleft(self, e):
+        if self._size == self._maxlen:
+            raise Exception('Deque is full')
+        self._front = (self._front - 1) % self._maxlen
+        self._data[self._front] = e
+        self._size += 1
+
+    def remove(self, e):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        index = -1
+        for i in range(self._size):
+            if self._data[(self._front + i) % self._maxlen] == e:
+                index = i
+                break
+        if index == -1:
+            raise ValueError('Element not found')
+        for i in range(index, self._size-1):
+            self._data[(self._front + i) % self._maxlen] = self._data[(self._front + i + 1) % self._maxlen]
+        self._data[(self._front + self._size - 1) % self._maxlen] = None
+        self._size -= 1
+        return e
+
+    def delete_first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        answer = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % self._maxlen
+        self._size -= 1
+        return answer
+
+    def delete_last(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        back = (self._front + self._size - 1) % self._maxlen
+        answer = self._data[back]
+        self._data[back] = None
+        self._size -= 1
+        return answer
+    
+    def pop(self):
+        return self.delete_last()
+
+    def popleft(self):
+        return self.delete_first()
+
+    def add_first(self, e):
+        self.appendleft(e)
+
+    def add_last(self, e):
+        self.append(e)
 
 
-class ArrayDequeMaxlen(): pass
-        
-                
-        
-        
+
    
+    
+
+
--- main.py
+++ main.py
@@ -7,31 +7,27 @@ Created on Sun Mar  5 18:34:20 2023
 
 from ArrayDeque import ArrayDequeMaxlen
 
-     
-        
 AQM = ArrayDequeMaxlen(20)
 
-
-print('Adding last')
+# adding last
+print('\nAdding last')
 for i in range(100):
     AQM.append(i)
-    print (i, AQM._data)
-    
+    print(i, AQM._data)
+
 print('\nDelete 80', AQM.remove(80), AQM._data, AQM._front)
-    
 
-
-    
-print ('\nAdding first')
+# adding first
+print('\nAdding first')
 for i in range(20, 10, -1):
     AQM.appendleft(i)
-    print (i, AQM._data)
-    
-    
-print(AQM._front)
-    
+    print(i, AQM._data)
 
-    
+print(AQM._front)
+
+# performing the removals
+
 print('\nPerforming the removals')
 while not AQM.is_empty():
-    print ('Remove first', AQM[0], AQM.popleft(), 'Remove last', AQM[-1],  AQM.pop())
+    AQM.pop()
+    print('removing last', AQM.pop(), AQM._data)
